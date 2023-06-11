0e3b6ea44e01365a0a01b039c89f750bad192977
Daria Sudareva <daria@sliday.com>
2023-06-12 02:32:07 +0700

    add all pages

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('./dist'));
+
+app.get('*', (req, res) => {
+  res.sendFile('/dist/index.html', {root: __dirname})
+})
+
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
--- src/main.js
+++ src/main.js
@@ -6,10 +6,15 @@ import profileTitle from './partials/profileTitle/profileTitle.hbs';
 import input from './partials/input/input.hbs';
 import link from './partials/link/link.hbs';
 import avatar from './partials/avatar/avatar.hbs';
+import profileText from './partials/profileText/profileText.hbs';
 import login from './templates/login/login.hbs';
 import registration from './templates/register/register.hbs';
 import profile from './templates/profile/profile.hbs';
-import profileText from './partials/profileText/profileText.hbs';
+import profileChange from './templates/profile-change/profile-change.hbs';
+import passwordChange from './templates/password-change/password-change.hbs';
+import main from './templates/main/main.hbs';
+import notFound from './templates/404/404.hbs';
+import serverError from './templates/500/500.hbs';
 
 Handlebars.registerPartial('button', button);
 Handlebars.registerPartial('modalTitle', modalTitle);
@@ -22,11 +27,40 @@ Handlebars.registerPartial('profileText', profileText);
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.querySelector('#app');
 
+  root.innerHTML = '';
+
+  const mainPage = main();
   const registrationPage = registration();
   const loginPage = login();
   const profilePage = profile();
+  const profileChangePage = profileChange();
+  const passwordChangePage = passwordChange();
+  const notFoundPage = notFound();
+  const serverErrorPage = serverError();
 
-  root.innerHTML = profilePage;
+  const getCurrentPage = () => {
+    switch (window.location.pathname) {
+      case '/login':
+        return root.innerHTML = loginPage;
+      case '/join-us':
+        return root.innerHTML = registrationPage;
+      case '/profile':
+        return root.innerHTML = profilePage;
+      case '/data-change':
+        return root.innerHTML = profileChangePage;
+      case '/pass-change':
+        return root.innerHTML = passwordChangePage;
+      case '/404':
+        return root.innerHTML = notFoundPage;
+      case '/500':
+        return root.innerHTML = serverErrorPage;
+
+      default:
+        return root.innerHTML = mainPage;
+    }
+  }
+
+  getCurrentPage();
 })
 
 
